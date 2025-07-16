a9a46f07fea5eb7e8e80c8b8c5df95b632fc9b65
bloom <bloomillus@gmail.com>
2025-07-16 16:33:10 -0500

    Added styles to navbar, and footer, edited normalize css file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: process.env.NODE_ENV === 'production' ? { prefetchAll: true } : false,
-  site: 'http://localhost:4321/'
+  site: 'http://localhost:4321/',
+  integrations: [icon()]
 });
