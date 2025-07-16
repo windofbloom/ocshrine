e5e5925a8058fce7758a45031da67d6e41822405
bloom <bloomillus@gmail.com>
2025-07-15 20:51:58 -0500

    Added files and general seo configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: process.env.NODE_ENV === 'production' ? { prefetchAll: true } : false,
+  site: 'http://localhost:4321/'
+});
