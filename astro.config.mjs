504123d5b22e3349fb31c9ec830d0c6430f0d0f0
bloom <bloomillus@gmail.com>
2025-07-24 09:54:38 -0500

    Update 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   prefetch: process.env.NODE_ENV === 'production' ? { prefetchAll: true } : false,
-  site: 'http://localhost:4321/',
+  site: 'https://www.animapulchra.windofbloom.com/',
   integrations: [icon()]
 });
